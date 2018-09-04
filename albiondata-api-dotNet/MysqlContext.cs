ba30803d81bd41e621e422c5b91de84d2f5846ae
broderick.hyman <broderick.hyman@gentex.com>
2018-09-04 13:29:21 -0400

    Adding mysql context

---
+++ albiondata-api-dotNet/MysqlContext.cs
@@ -0,0 +1,13 @@
+﻿using AlbionData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace albiondata_api_dotNet
+{
+  public class MysqlContext : MainContext
+  {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseMySql(Program.SqlConnectionUrl);
+    }
+  }
+}
