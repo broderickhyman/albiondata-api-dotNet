9680064d54c4f9cefaa60dbae0f55b07fd3e3773
Broderick Hyman <broderickhyman@gmail.com>
2018-09-05 23:41:19 -0400

    Market prices working exactly like golang version

--- albiondata-api-dotNet/Controllers/PricesController.cs
+++ albiondata-api-dotNet/Controllers/PricesController.cs
@@ -1,7 +1,9 @@
 ﻿using AlbionData.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +15,8 @@ namespace albiondata_api_dotNet.Controllers
   public class PricesController : ControllerBase
   {
     private readonly MainContext context;
+    private static readonly string[] AuctionTypes = new[] { "request", "offer" };
+    private static readonly int[] AggregateTypes = new[] { 0, 1 };
 
     public PricesController(MainContext context)
     {
@@ -22,18 +26,75 @@ namespace albiondata_api_dotNet.Controllers
     [HttpGet("{itemId}")]
     public ActionResult<IEnumerable<MarketResponse>> Get([FromRoute]string itemId)
     {
-      //return context.MarketOrders.Where(x => x.ItemTypeId == itemId).Take(10).ToArray();
-      var groups = context.MarketOrders
-        .Where(x => x.ItemTypeId == itemId)
-        .Take(10).ToArray()
-        .GroupBy(x => new { x.ItemTypeId, x.LocationId });
-      return groups.Select(x => new MarketResponse
+      var items = context.MarketOrders
+        .Where(x => EF.Functions.Like(x.ItemTypeId, itemId.Replace('*', '%')) && x.UpdatedAt > DateTime.UtcNow.AddDays(-2) && !x.DeletedAt.HasValue)
+        .ToArray();
+      Debug.WriteLine(items.Length);
+      var groups = items.GroupBy(x => new { x.ItemTypeId, x.LocationId });
+      var responses = new List<MarketResponse>();
+      foreach (var group in groups)
       {
-        ItemTypeId = x.Key.ItemTypeId,
-        City = x.Key.LocationId.ToString(),
-        SellPriceMin = x.Min(y => y.UnitPriceSilver),
-        SellPriceMax = x.Max(y => y.UnitPriceSilver)
-      }).ToArray();
+        var dict = new Dictionary<string, UpdatedAggregate>();
+        foreach (var auctionType in AuctionTypes)
+        {
+          foreach (var aggregateType in AggregateTypes)
+          {
+            var key = $"{auctionType}:{aggregateType}";
+            dict[key] = new UpdatedAggregate
+            {
+              UpdatedAt = DateTime.MinValue,
+              Value = 0
+            };
+          }
+        }
+        foreach (var auctionType in AuctionTypes)
+        {
+          foreach (var order in group.Where(x => string.Equals(x.AuctionType, auctionType, StringComparison.OrdinalIgnoreCase)))
+          {
+            foreach (var aggregateType in AggregateTypes)
+            {
+              var key = $"{auctionType}:{aggregateType}";
+              var current = dict[key];
+              var u = order.UpdatedAt;
+              var updatedGroup = new DateTime(u.Year, u.Month, u.Day, u.Hour, u.Minute, 0);
+              if (updatedGroup > current.UpdatedAt)
+              {
+                current.UpdatedAt = updatedGroup;
+                current.Value = order.UnitPriceSilver;
+              }
+              else if (updatedGroup == current.UpdatedAt)
+              {
+                if ((aggregateType == 0 && order.UnitPriceSilver < current.Value) || (aggregateType == 1 && order.UnitPriceSilver > current.Value))
+                {
+                  current.Value = order.UnitPriceSilver;
+                }
+              }
+            }
+          }
+        }
+
+        responses.Add(new MarketResponse
+        {
+          ItemTypeId = group.Key.ItemTypeId,
+          City = group.Key.LocationId.ToString(),
+          SellPriceMin = dict["offer:0"].Value,
+          SellPriceMinDate = dict["offer:0"].UpdatedAt,
+          SellPriceMax = dict["offer:1"].Value,
+          SellPriceMaxDate = dict["offer:1"].UpdatedAt,
+          BuyPriceMin = dict["request:0"].Value,
+          BuyPriceMinDate = dict["request:0"].UpdatedAt,
+          BuyPriceMax = dict["request:1"].Value,
+          BuyPriceMaxDate = dict["request:1"].UpdatedAt
+        });
+      }
+      responses.Sort((a, b) => string.Compare(a.City, b.City));
+      return responses;
+    }
+
+    private class UpdatedAggregate
+    {
+      public DateTime UpdatedAt;
+      public ulong Value;
     }
   }
 }
